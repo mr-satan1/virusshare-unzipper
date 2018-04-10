ab5b75986375283959c3078a19ac1d882d815bdb
mr-satan1 <>
2018-04-10 15:55:12 -0700

    Added AWS Glacier archiving support

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # virusshare-unzipper
-This simple application is targeted for individuals who deal with large VirusShare.com malware samples. 
+This simple application is targeted for individuals who deal with large VirusShare.com malware samples.
 
 This application does bulk unzipping of password protected malware zip samples. Sure as hell beats typing 'infected' dozens of times...
 
-## Usage
+In addition to bulk unzipping operations, you can also provide this application your AWS Glacier VaultName and have the malware samples be uploaded for long term archiving purposes.
+
+## Basic Usage
 Provide the input directory argument with a directory containing the VirusShare.com zip files. Provide an output directory for the location where the samples will be uncompressed.
 
 
 `python malunzip.py --indir ~/Malware/ --outdir ~/Malware/samples/`
 
+### Archving to AWS Glacier
+Note - you will need to ensure that the boto3 library is installed and that you have configured your the IAM role your client uses to have the ability to upload to Glacier VaultName ARN.
+
+`python malunzip.py --indir ~/Malware/ --outdir ~/Malware/samples/ --glacier $VAULT_NAME`
--- malunzip.py
+++ malunzip.py
@@ -1,6 +1,8 @@
 from zipfile import ZipFile
+from shutil import make_archive
 import os
 import argparse
+import sys
 
 # Currently optimized for VirusShare.com files.
 ## If you want to add for other services,
@@ -11,13 +13,28 @@ parser.add_argument('--indir', help='Input directory containing zip samples from
                     required=True)
 parser.add_argument('--outdir', help='Output directory for the malware samples.',
                     required=True)
+parser.add_argument('--glacier', help='Provide the AWS Glacier Vault name for long term malware storage. Note - uploads take ~24hrs to complete.')
+
+
 args = parser.parse_args()
 
-def unzipFile(file):
-    if os.path.exists(file):
+def unzipFile(filename):
+    if os.path.exists(filename):
         with ZipFile(file) as zippedFile:
             zippedFile.extractall(args.outdir, pwd=TypicalMalwareSharingPassword.encode())
 
+def zipDirGlacier(archiveName, archiveDir):
+    archName = os.path.expanduser(os.path.join('~', archiveName))
+    make_archive(archName, 'zip', archiveDir)
+    print('[*] Archive done! Sending to Glacier.')
+    try:
+        archName = os.path.expanduser(os.path.join('~', archiveName)) + '.zip'
+        with open(archName, 'rb') as upload:
+            response = client.upload_archive(vaultName=args.glacier,
+            archiveDescription=archName, body=upload)
+        print('[*] Archived to Glacier! It takes ~24 hours for the archive to be uploaded!')
+    except Exception as e:
+        print(e)
 
 def listZip(directory):
     global UnzipCounter
@@ -32,7 +49,24 @@ def listZip(directory):
             unzipFile(FullFilePath)
 
 if __name__ == "__main__":
-    if os.path.exists(args.indir):
+    if args.glacier:
+        try:
+            import boto3
+            client = boto3.client('glacier')
+            print("[*] Using Glacier VaultName: %s" % args.glacier)
+            accept = input('>>> Would you like to archive everything in >> %s << to AWS Glacier VaultName: %s (y/n)? ' % (args.indir, args.glacier)).lower()
+            if accept.startswith('y'):
+                print('[*] Beginning Glacier upload process...')
+                zipDirGlacier(args.glacier, args.indir)
+            else:
+                print("User input != 'y'. Exiting.")
+                sys.exit(1)
+        except Exception as e:
+            print(e)
+
+
+
+    elif os.path.exists(args.indir):
         listZip(args.indir)
         print("[*] Completed unzip operations on %s amount of files" % UnzipCounter)
         print('[*] Your malware is ready: %s' % args.outdir)
